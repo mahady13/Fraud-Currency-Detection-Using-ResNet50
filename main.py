08b391ad0082063227c57762b2293378e8e4b186
mahady13 <mohiuddinmahady@gmail.com>
2026-07-22 02:36:28 +0600

    added the label encoder & transforms

--- main.py
+++ main.py
@@ -29,3 +29,13 @@ def load_assets():
 model=load_assets()
 model.to(device)
 model.eval()
+
+#label encoder loaded
+label_encoder = joblib.load("label_encoder.pkl")
+
+#transform function created
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
